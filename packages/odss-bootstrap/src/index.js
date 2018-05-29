633041804d6f8f79e47402cbac86e0799af051c0
Piotr Smialkowski <p.smialkowski@gmail.com>
2018-05-29 05:45:46 +0200

    [bootstrap] Add runner option

--- packages/odss-bootstrap/src/index.js
+++ packages/odss-bootstrap/src/index.js
@@ -1,18 +1,24 @@
 import { ready } from 'sjs-dom';
 import { Framework } from 'odss-framework';
 
-
-export async function boot(options = {}) {
+export async function boot(options = {}, runner=asyncRunner) {
     await ready();
 
     let framework = new Framework(options.properties);
     framework.start();
     let bundles = options.bundles || [];
+    runner(framework, bundles)
+    return framework;
+}
+
+export async function asyncRunner(framework, bundles) {
     await Promise.all(bundles.map(location =>
         framework.installBundle(location, true)
     ));
-    // for(let bundle of bundles){
-    //     await framework.installBundle(bundle, true);
-    // }
-    return framework;
+}
+
+export async function syncRunner(framework, bundles){
+    for(let bundle of bundles){
+        await framework.installBundle(bundle, true);
+    }
 }
