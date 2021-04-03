b35849786206594a7ab70247cf39c957f672e039
Piotr Smialkowski <p.smialkowski@gmail.com>
2021-04-03 08:30:22 +0200

    Remove api package

--- packages/odss-api/src/cm.ts
+++
@@ -1,9 +0,0 @@
-export const IManagerService = 'odss-api.IManagerService';
-
-export interface IManagerService {
-    updated(properties: object);
-}
-
-export abstract class ManagerService implements IManagerService {
-    abstract updated(properties: object);
-}
