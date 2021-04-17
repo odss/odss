9e8de3736895cb3ffca84a26032270b135e2fe8d
Piotr Smialkowski <p.smialkowski@gmail.com>
2021-04-17 11:10:20 +0200

    Add package odss.cdi.example

--- packages/odss.cdi.example/src/index.ts
+++ packages/odss.cdi.example/src/index.ts
@@ -1,9 +1,10 @@
 import { IBundleContext, IServiceReference } from '@odss/common';
-import { Component, Bind, Unbind, Provide, Validate, Invalidate, Property } from '@odss/cdi';
+import { Component, Bind, Unbind, Provides, Validate, Invalidate, Property } from '@odss/cdi-decorators';
 
 
-const ISpellDictionary = 'odss.cdi.example.ISpellDictionary';
-const ISpellChecker = 'odss.cdi.example.Checker';
+class ISpellDictionaryService {};// = 'odss.cdi.example.ISpellDictionary';
+class ISpellCheckerService{};// = 'odss.cdi.example.Checker';
+
 
 interface ISpellDictionary {
     checkWord(word: string): boolean;
@@ -14,30 +15,41 @@ interface ISpellChecker {
 }
 
 @Component()
-@Provide(ISpellDictionary)
-@Property('language', 'en')
+@Provides(ISpellDictionaryService, {language:'en'})
 export class EnglishSpellDictionary {
+
+    // @Property('a.b.c')
+    private name: string = '';
+
     private dictionary: string[] = [];
 
+    @Validate()
     activate(ctx: IBundleContext){
         this.dictionary = ["hello", "world", "welcome", "to", "the", "odss", "test"];
+        console.log('A EnglishSpellDictionary has been started');
     }
+
+    @Invalidate()
     deactivate(ctx: IBundleContext){
         this.dictionary = [];
     }
+
     checkWord(word: string): boolean {
         return this.dictionary.indexOf(word) !== -1;
     }
 }
 
-@Component(ISpellDictionary)
-@Property('language', 'pl')
+@Component()
+@Provides(ISpellDictionaryService, {'language': 'pl'})
 export class PolishSpellDictionary {
     private dictionary: string[] = [];
 
+    @Validate()
     activate(ctx: IBundleContext){
         this.dictionary = ["witaj", "świecie", "witaj", "w", "odss", "tescie"];
+        console.log('A PolishSpellDictionary has been started');
     }
+    @Invalidate()
     deactivate(ctx: IBundleContext){
         this.dictionary = [];
     }
@@ -47,31 +59,34 @@ export class PolishSpellDictionary {
 }
 
 
-@Component(ISpellChecker)
+@Component()
+@Provides(ISpellCheckerService)
 export class SpellChecker {
 
     private languages: Map<string, ISpellDictionary> = new Map();
 
-    @Bind(ISpellDictionary)
+    @Bind(ISpellDictionaryService)
     add(service: ISpellDictionary, ref: IServiceReference) {
-        let language = ref.getProperty('language');
+        let language = ref.getProperty<string>('language');
+        console.log(`SpellChecker.add(${language})`);
         this.languages.set(language, service);
 
     }
-    @Unbind(ISpellDictionary)
+    @Unbind(ISpellDictionaryService)
     remove(service: ISpellDictionary, ref: IServiceReference) {
-        let language = ref.getProperty('language');
+        let language = ref.getProperty<string>('language');
+        console.log(`SpellChecker.remove(${language})`);
         this.languages.delete(language);
     }
 
     @Validate()
     activate(): void {
-        console.log('A spell checker has been started')
+        console.log('A spell checker has been started');
     }
 
     @Invalidate()
     deactivate(): void {
-        console.log('A spell checker has been stopped')
+        console.log('A spell checker has been stopped');
     }
 
     check(message: string, language: string = 'en'): boolean {
@@ -83,3 +98,10 @@ export class SpellChecker {
     }
 }
 
+
+@Component()
+export class SpellCheckerTest {
+    constructor(private checker: ISpellCheckerService) {
+        console.log('SpellCheckerTest', checker);
+    }
+}
