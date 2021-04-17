cb2abce0d125cba064ea404c54f4cdf2b1d5a3a1
Piotr Smialkowski <p.smialkowski@gmail.com>
2021-04-17 11:04:47 +0200

    Remove packages: odss.dev.ui.terminal and odss.dev.ui.core

--- packages/odss.dev.ui.core/src/view.js
+++
@@ -1,7 +0,0 @@
-export function createView(html) {
-    var container = document.createElement('div');
-    container.innerHTML = html.trim();
-    var $dom = container.firstChild;
-    document.body.appendChild($dom);
-    return $dom;
-}
